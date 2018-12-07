bc5db28bd7360201e83f2ed4a8d236da4f3e2e91
PaxSentry <paxsentry@gmail.com>
2018-12-07 14:18:44 +0000

    Started

---
+++ Survey-Ships/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Survey_Ships
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+         Console.ReadKey();
+      }
+   }
+}
