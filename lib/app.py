7c8099aa8e449ec190b68410308fc60e6c8dcd34
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-01-13 16:35:15 -0500

    added pseudocode for ship instruction

--- lib/app.py
+++ lib/app.py
@@ -15,32 +15,35 @@ def take_inputs () :
     # # orientation (N, S, E, W) = string, Uppercase
     shipPosition = str(input("Type in grid point coordinates and ship orientation i.e. X Y N: ")).upper()
     shipPositionList = ''.join(shipPosition).split() 
-    print(shipPositionList)
 
     # collect grid point coordinates into integers of X & Y
-    X = shipPositionList[0]
-    Y = shipPositionList[1]
-    shipOrientation = shipPositionList[2]
+    X = int(shipPositionList[0])
+    Y = int(shipPositionList[1])
+    # collect ship orientation string in ship position
+    shipOrientation = str(shipPositionList[2])
     print(f'X: {X}\nY: {Y}\nShip Orientation: {shipOrientation}')
-
 
 
     # input 3: Ship instruction (2 lines per ship)
     # ship instruction : string of the letters "L", "R", and "F" on one line, < 100 characters 
     shipInstruction = str(input("Type in Ship instructions i.e. LRF: ")).upper()   
     print(shipInstruction)
+    # analyze ship instructions
+    # make sure input is only LRF
+    # # R: the ship turns right 90 degrees and remains on the current grid point.
+    # # if R, N --> E 
+    # # if R, E --> S 
+    # # if R, S --> W 
+    # # if R, W --> N 
+    # # L: the ship turns left 90 degrees and remains on the current grid point.
+    # # if L, N --> W
+    # # if L, W --> S
+    # # if L, S --> E
+    # # if L, E --> N
+    # # Forward: the ship moves forward one grid point in the direction of the current orientation and maintains the same orientation. The direction North corresponds to the direction from grid point (x, y) to grid point (x, y+1) and the direction east corresponds to the direction from grid point (x, y) to grid point (x+1, y).
+    # # if F, X = X+1
+     # # if F, Y = Y+1
 
-
-# def analyze_inputs () :
-#     take_inputs()
-#     # collect grid point coordinates into integers of X & Y
-#     # collect ship orientation string in ship position
-#     # analyze ship instructions
-#     # # Left: the ship turns left 90 degrees and remains on the current grid point.
-#     # # Right: the ship turns right 90 degrees and remains on the current grid point.
-#     # # Forward: the ship moves forward one grid point in the direction of the current orientation and maintains the same orientation. The direction North corresponds to the direction from grid point (x, y) to grid point (x, y+1) and the direction east corresponds to the direction from grid point (x, y) to grid point (x+1, y).
-    
-#     print(shipInstruction)
 
 take_inputs()
 
