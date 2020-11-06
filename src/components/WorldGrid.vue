ee224b4ac2b11333c9a7e49450dd75a81c208183
Mateusz Rybczonek <m.rybczonek@gmail.com>
2020-11-06 08:17:56 -0500

    refactor(grid): create grid dynamically

--- src/components/WorldGrid.vue
+++ src/components/WorldGrid.vue
@@ -62,32 +62,16 @@ export default {
     },
 
     gridItems () {
-      return [
-        { id: 1, x: 0, y: 3 },
-        { id: 2, x: 1, y: 3 },
-        { id: 3, x: 2, y: 3 },
-        { id: 4, x: 3, y: 3 },
-        { id: 5, x: 4, y: 3 },
-        { id: 6, x: 5, y: 3 },
-        { id: 7, x: 0, y: 2 },
-        { id: 8, x: 1, y: 2 },
-        { id: 9, x: 2, y: 2 },
-        { id: 10, x: 3, y: 2 },
-        { id: 11, x: 4, y: 2 },
-        { id: 12, x: 5, y: 2 },
-        { id: 13, x: 0, y: 1 },
-        { id: 14, x: 1, y: 1 },
-        { id: 15, x: 2, y: 1 },
-        { id: 16, x: 3, y: 1 },
-        { id: 17, x: 4, y: 1 },
-        { id: 18, x: 5, y: 1 },
-        { id: 19, x: 0, y: 0 },
-        { id: 20, x: 1, y: 0 },
-        { id: 21, x: 2, y: 0 },
-        { id: 22, x: 3, y: 0 },
-        { id: 23, x: 4, y: 0 },
-        { id: 24, x: 5, y: 0 }
-      ]
+      let id = 0
+      const gridItemsArray = []
+
+      for (let y = 3; y > -1; y--) {
+        for (let x = 0; x < 6; x++) {
+          gridItemsArray.push({ id: id++, x, y })
+        }
+      }
+
+      return gridItemsArray
     }
   },
 
