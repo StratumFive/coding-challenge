fbab281528b38c5d1f565b87afc1068b49698bec
Mateusz Rybczonek <m.rybczonek@gmail.com>
2020-11-06 05:10:13 -0500

    feat(vessel): create vessel component

---
+++ src/components/Vessel.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="vessel"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    heading: {
+      type: String,
+      required: true
+    },
+
+    position: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.vessel {
+  position: absolute;
+  width: 30px;
+  height: 50px;
+  top: 335px;
+  left: 35px;
+  background: #A9A9A9;
+  border-radius: 10px;
+}
+
+.vessel:before {
+  content: "";
+  position: absolute;
+  right: 0;
+  top: -10px;
+  width: 0;
+  height: 0;
+  border-left: 15px solid transparent;
+  border-right: 15px solid transparent;
+  border-bottom: 15px solid #A9A9A9;
+}
+</style>
