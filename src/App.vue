96f88a8d6dc0996f4ae18e48781705ebd64220cc
Serafín Maza Domínguez <serafinmazadominguez@helium.home>
2020-11-08 12:30:44 +0000

    feat(app): Removed scaffolded components and added basic template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="min-h-screen gradient-ocean">
+    <div class="grid grid-cols-2 gap-8 p-8">
+      <div class="bg-white rounded-lg p-6 border shadow-xl">Input</div>
+      <div class="bg-white rounded-lg p-6 border shadow-xl">Output</div>
+      <div class="bg-white col-span-2 rounded-lg p-6 border shadow-xl">
+        Grid
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.gradient-ocean {
+  background-image: linear-gradient(135deg, #90cdf4 35%, #38b2ac 100%);
 }
 </style>
