05971dfeb0ffaa54dd092c1dfb420c1069a54ed7
l-k-mist <dylan@vandenbosch.co.za>
2021-02-26 10:29:08 +0200

    refactor: use vuetify text-area with same id for tests.

--- src/components/TextBasedSolution.vue
+++ src/components/TextBasedSolution.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <div>I'm a text-based solution</div>
-    <v-btn color="success">
-      <v-icon>mdi-account-box-outline</v-icon> text</v-btn
-    >
-    <input id="text-input" type="text" />
+    <v-textarea
+      id="text-input"
+      label="Input"
+      v-model="commandInput"
+    ></v-textarea>
+    <v-btn color="success">Calculate Ship Positions</v-btn>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      commandInput:
+        "5 3 \n 1 1 E \n RFRFRFRF \n\n 3 2 N \n FRRFLLFFRRFLL \n\n 0 3 W \n LLFFFLFLFL",
+    };
+  },
+};
 </script>
 
 <style scoped></style>
