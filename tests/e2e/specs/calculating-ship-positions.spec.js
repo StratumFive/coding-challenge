cc83f76a33736459eef94cef6a28e712eb62361c
l-k-mist <dylan@vandenbosch.co.za>
2021-02-26 11:16:40 +0200

    spec: failing test for max co-ordinate parsing from command-string

--- tests/e2e/specs/calculating-ship-positions.spec.js
+++ tests/e2e/specs/calculating-ship-positions.spec.js
@@ -1,7 +1,7 @@
 describe("Calculating ship positions", () => {
   it("calculates the final position and orientation of a ship", () => {
     const sampleInput =
-      "5 3 \n 1 1 E \n RFRFRFRF \n\n 3 2 N \n FRRFLLFFRRFLL \n\n 0 3 W \n LLFFFLFLFL";
+      "5 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL";
     cy.visit("/");
     cy.get("#text-input").type(sampleInput);
     // TODO Refactor this test for greater specificity on the order of outputs: ie An edge-case could pass this test with incorrect order of ships in the output.
--- tests/unit/helpers/parseCommand.spec.js
+++ tests/unit/helpers/parseCommand.spec.js
@@ -1,7 +1,14 @@
 import parseCommand from "@/helpers/parseCommand";
 
 describe("parseCommand", () => {
-  it("returns an array", () => {
+  it("returns an object", () => {
     expect(typeof parseCommand("Hello\nWorld")).toBe("object");
+  });
+  it("extracts maxY and maxX from the command", () => {
+    const command =
+      "5 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL";
+    const parsedCommand = parseCommand(command);
+    expect(parsedCommand.maxY).toBe(5);
+    expect(parsedCommand.maxX).toBe(3);
   });
 });
