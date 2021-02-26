da7eb8bc8b1eae13785db36a7ea7f987a65236d3
l-k-mist <dylan@vandenbosch.co.za>
2021-02-26 15:28:04 +0200

    green: Ship class exists as object

--- src/helpers/Ship.js
+++ src/helpers/Ship.js
@@ -1,1 +1,7 @@
-
+export default class Ship {
+  constructor(attributes) {
+    this.startingCoords = attributes.startingCoords;
+    this.startingDirection = attributes.startingDirection;
+    this.commands = attributes.commands;
+  }
+}
