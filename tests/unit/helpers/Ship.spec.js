76613dfb876de40758ddfc03ab6fe3f7aae775dc
l-k-mist <dylan@vandenbosch.co.za>
2021-02-26 16:05:13 +0200

    refactor: beforeEach for test

--- tests/unit/helpers/Ship.spec.js
+++ tests/unit/helpers/Ship.spec.js
@@ -1,15 +1,18 @@
 import Ship from "@/helpers/Ship";
 
 describe("The Ship Class", () => {
-  const shipValuesFromParsedInput = {
-    commands: "RFRFRFRF",
-    startingCoords: {
-      x: 1,
-      y: 1,
-    },
-    startingDirection: "E",
-  };
-  const testShip = new Ship(shipValuesFromParsedInput);
+  let testShip;
+  beforeEach(() => {
+    const shipValuesFromParsedInput = {
+      commands: "RFRFRFRF",
+      startingCoords: {
+        x: 1,
+        y: 1,
+      },
+      startingDirection: "E",
+    };
+    testShip = new Ship(shipValuesFromParsedInput);
+  });
   it("exists as an object", () => {
     expect(typeof testShip).toBe("object");
   });
