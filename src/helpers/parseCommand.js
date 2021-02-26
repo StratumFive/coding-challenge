c9c0c28710585637b4771b843865ffa0517c5c10
l-k-mist <dylan@vandenbosch.co.za>
2021-02-26 11:26:10 +0200

    green: tests pass

--- src/helpers/parseCommand.js
+++ src/helpers/parseCommand.js
@@ -1,3 +1,12 @@
 export default (command) => {
-  return command;
+  const regexForCoordinates = /\d+ \d+\n/gm;
+  const coordinates = command
+    .match(regexForCoordinates)[0]
+    .replace("\n", "")
+    .split(" ");
+  console.log("coordinates", coordinates);
+  return {
+    maxY: Number(coordinates[0]),
+    maxX: Number(coordinates[1]),
+  };
 };
--- tests/unit/helpers/parseCommand.spec.js
+++ tests/unit/helpers/parseCommand.spec.js
@@ -4,7 +4,7 @@ describe("parseCommand", () => {
   it("returns an object", () => {
     expect(typeof parseCommand("Hello\nWorld")).toBe("object");
   });
-  it("extracts maxY and maxX from the command", () => {
+  it.only("extracts maxY and maxX from the command", () => {
     const command =
       "5 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL";
     const parsedCommand = parseCommand(command);
