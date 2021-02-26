aa733deb3d839c2c16e055a9cb5ebd73aab87770
l-k-mist <dylan@vandenbosch.co.za>
2021-02-26 12:27:55 +0200

    green: but ugly

--- src/helpers/parseCommand.js
+++ src/helpers/parseCommand.js
@@ -5,9 +5,22 @@ export default (command) => {
     .replace("\n", "")
     .split(" ");
   const commandWithoutCoordinates = command.replace(regexForCoordinates, "");
+  const shipsRaw = commandWithoutCoordinates.split("\n\n");
+  console.log("shipsRaw", shipsRaw);
+  const ships = shipsRaw.map((ship) => {
+    console.log("ships - ship", ship);
+    return {
+      startingCoords: {
+        y: Number(ship.match(/\d+ \d+ [NSEW]\n/gm)[0].split(" ")[0]),
+        x: Number(ship.match(/\d+ \d+ [NSEW]\n/gm)[0].split(" ")[1]),
+      },
+    };
+  });
+
+  console.log("shipsRaw", shipsRaw);
   return {
     maxY: Number(coordinates[0]),
     maxX: Number(coordinates[1]),
-    ships: [{}],
+    ships,
   };
 };
