ed16f5f1b8c91d61087e741b92c3f5cc01ce7392
l-k-mist <dylan@vandenbosch.co.za>
2021-02-26 16:01:05 +0200

    green: turn left

--- src/helpers/Ship.js
+++ src/helpers/Ship.js
@@ -4,6 +4,14 @@ export default class Ship {
     this.direction = attributes.startingDirection;
     this.commands = attributes.commands;
   }
-  turnLeft() {}
+  turnLeft() {
+    // Deal with the case of "N", which is at index 0 of CLOCKWISE_DIRECTIONS
+    if (this.direction === "N") {
+      this.direction = "W";
+      return;
+    }
+    this.direction =
+      CLOCKWISE_DIRECTIONS[CLOCKWISE_DIRECTIONS.indexOf(this.direction) - 1];
+  }
 }
 const CLOCKWISE_DIRECTIONS = ["N", "E", "S", "W"];
