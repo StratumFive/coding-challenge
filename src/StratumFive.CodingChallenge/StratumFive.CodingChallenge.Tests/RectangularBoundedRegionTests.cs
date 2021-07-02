cc03d43d9e77ca87f2bae7df6381e8f6c9348f26
Richard Smothers <1252123+kharenis@users.noreply.github.com>
2021-07-02 18:28:33 +0100

    Added basic tests

--- src/StratumFive.CodingChallenge/StratumFive.CodingChallenge.Tests/RectangularBoundedRegionTests.cs
+++ src/StratumFive.CodingChallenge/StratumFive.CodingChallenge.Tests/RectangularBoundedRegionTests.cs
@@ -1,13 +1,44 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StratumFive.CodingChallenge.Core;
+using StratumFive.CodingChallenge.Core.Interfaces;
 
 namespace StratumFive.CodingChallenge.Tests
 {
     [TestClass]
     public class RectangularBoundedRegionTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IBoundedRegion boundedRegion;
+        [TestInitialize]
+        public void Init()
         {
+            boundedRegion = new RectangularBoundedRegion(new IntVector2(10, 10));
+        }
+
+        [TestMethod]
+        public void When_ExpectedValues_IsInRegion()
+        {
+            var position = new IntVector2(5, 5);
+            var isInRegion = boundedRegion.IsInRegion(position);
+
+            Assert.IsTrue(isInRegion);
+        }
+
+        [TestMethod]
+        public void When_NegativeValues_IsNotInRegion()
+        {
+            var position = new IntVector2(-4, 8);
+            var isInRegion = boundedRegion.IsInRegion(position);
+
+            Assert.IsFalse(isInRegion);
+        }
+
+        [TestMethod]
+        public void When_TooLargeValues_IsNotInRegion()
+        {
+            var position = new IntVector2(2, 100);
+            var isInRegion = boundedRegion.IsInRegion(position);
+
+            Assert.IsFalse(isInRegion);
         }
     }
 }
