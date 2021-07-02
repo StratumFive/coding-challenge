21ccba7950d39407c13d89353a99fdf237aab9d9
Richard Smothers <1252123+kharenis@users.noreply.github.com>
2021-07-02 18:08:15 +0100

    Added logic & test projects

---
+++ src/StratumFive.CodingChallenge/StratumFive.CodingChallenge.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StratumFive.CodingChallenge.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
