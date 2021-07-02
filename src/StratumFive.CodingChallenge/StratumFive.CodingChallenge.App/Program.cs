c425a2507d60fa2ffa6daa6b904faf185ae83bf1
Richard Smothers <1252123+kharenis@users.noreply.github.com>
2021-07-02 19:08:33 +0100

    Added gnarly string parsing to Main

--- src/StratumFive.CodingChallenge/StratumFive.CodingChallenge.App/Program.cs
+++ src/StratumFive.CodingChallenge/StratumFive.CodingChallenge.App/Program.cs
@@ -11,16 +11,26 @@ namespace StratumFive.CodingChallenge.App
             if (args.Length > 0)
             {
                 var regionBounds = GetBoundedRegion(args[0].Split(' '));
-
                 IWarningProvider warningProvider = new WarningProvider();
 
+                for(var i = 1; i < args.Length; i+=2)
+                {
+                    var shipInitialPositionAndHeading = args[i].Split(' ');
+                    Ship ship = new Ship(
+                        new IntVector2(int.Parse(shipInitialPositionAndHeading[0]),
+                        int.Parse(shipInitialPositionAndHeading[1])),
+                        HeadingFactory.GetHeading(shipInitialPositionAndHeading[2]),
+                        regionBounds,
+                        warningProvider);
 
+                    ship.ExecuteInstructions(args[i + 1]);
+                }
             }
             else
             {
                 Console.WriteLine("Args missing!");
             }
-            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
 
         static IBoundedRegion GetBoundedRegion(string[] parameters)
