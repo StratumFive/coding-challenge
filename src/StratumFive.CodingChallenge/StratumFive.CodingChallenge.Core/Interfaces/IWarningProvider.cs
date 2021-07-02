960a20188c74c65f6d1d3d5b9df8dfc806e36eb7
Richard Smothers <1252123+kharenis@users.noreply.github.com>
2021-07-02 18:46:52 +0100

    Fleshed out warning provider

--- src/StratumFive.CodingChallenge/StratumFive.CodingChallenge.Core/Interfaces/IWarningProvider.cs
+++ src/StratumFive.CodingChallenge/StratumFive.CodingChallenge.Core/Interfaces/IWarningProvider.cs
@@ -9,5 +9,10 @@ namespace StratumFive.CodingChallenge.Core.Interfaces
     public interface IWarningProvider
     {
         public bool AnyWarningsForPosition(IntVector2 position);
+        /// <summary>
+        /// Registers a warning against a given position
+        /// </summary>
+        /// <param name="position"></param>
+        public void RegisterWarningForPosition(IntVector2 position);
     }
 }
--- src/StratumFive.CodingChallenge/StratumFive.CodingChallenge.Core/WarningProvider.cs
+++ src/StratumFive.CodingChallenge/StratumFive.CodingChallenge.Core/WarningProvider.cs
@@ -9,9 +9,14 @@ namespace StratumFive.CodingChallenge.Core
 {
     public class WarningProvider : IWarningProvider
     {
+        private static HashSet<(int posX, int posY)> warnings = new HashSet<(int posX, int posY)>();
         public bool AnyWarningsForPosition(IntVector2 position)
         {
-            throw new NotImplementedException();
+            return warnings.Contains((position.X, position.Y));
+        }
+        public void RegisterWarningForPosition(IntVector2 position)
+        {
+            warnings.Add((position.X, position.Y));
         }
     }
 }
