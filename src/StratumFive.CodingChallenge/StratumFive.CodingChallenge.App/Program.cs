fa3e81a8c5902b1957e2d89db144682fc9475347
Richard Smothers <1252123+kharenis@users.noreply.github.com>
2021-07-02 18:06:40 +0100

    Created Project

---
+++ src/StratumFive.CodingChallenge/StratumFive.CodingChallenge.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StratumFive.CodingChallenge.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
